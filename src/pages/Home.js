3f4a42f33f088cff1c25d677839f11c852260240
TheLadsters <jeraldmags@gmail.com>
2021-11-29 00:02:51 +0800

    added pages folder

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Login from  './components/Login';
+import Sidebar from  './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+        <Login/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Table, Container, Row, Col} from "react-bootstrap";
+import './LandingPage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import * as IoIcons from 'react-icons/io';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+const LandingPage = () => {
+    return (
+    <>
+            <Routes>
+                <Route path="/" exact/>
+            </Routes>
+        
+        <Navbar />
+
+        <Container>
+            <Row className="text-end">
+                <Col md={4} className="mt-5 pt-3 pr-5 text-start">
+                <button className="btn btn-dark"><IoIcons.IoIosAddCircleOutline /> Add</button>
+                </Col>
+
+                <Col className="mt-5">
+                    <input type="text" className="my-3 mx-2" placeholder="Search"/>
+                    <button className=" btn btn-primary w-10 my-3 mx-2 text-dark">Search</button>
+                </Col>
+            </Row>
+        </Container>
+        
+        <Container>
+            <Table className="text-center">
+                <thead>
+                    <tr>
+                    <th>Deadline</th>
+                    <th>Customer's Name</th>
+                    <th>Order Code</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Downpayment</th>
+                    <th>Delivery Type</th>
+                    <th>Total Balance</th>
+                    <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>October 29, 2021</td>
+                    <td>Janmel Mangubat</td>
+                    <td>X000001</td>
+                    <td>21</td>
+                    <td>100</td>
+                    <td>1000</td>
+                    <td>Pick-up</td>
+                    <td>2100</td>
+                    <td>
+                        <button className="btn btn-success mx-1">
+                            Delivered
+                        </button>
+
+                        <button className="btn btn-dark mx-1">
+                            Edit
+                        </button>
+
+                        <button className="btn btn-danger mx-1">
+                            Delete
+                        </button>
+                    </td>
+
+                    </tr>
+                </tbody>
+            </Table>
+        </Container>
+    </>
+    )
+}
+
+export default LandingPage
