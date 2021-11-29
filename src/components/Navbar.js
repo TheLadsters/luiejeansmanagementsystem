9f16b43ffb704b192a827817974aeed0532b1a38
TheLadsters <jeraldmags@gmail.com>
2021-11-30 01:52:26 +0800

    improved navbar by adding clock and mobile optimization

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react';
+// import { Col, Row, Container } from 'react-bootstrap';
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import { SidebarData } from './SidebarData';
 import './Navbar.css';
 import { IconContext } from 'react-icons'
+import Clock from '../components/Clock';
 
 const Navbar = () => {
     const[sidebar, setSidebar] = useState(false);
@@ -13,11 +15,20 @@ const Navbar = () => {
     return (
         <>
         <IconContext.Provider value={{color: '#fff'}}>
-            <div className="navbar">
-                <Link to="#" className='menu-bars'>
-                    <FaIcons.FaBars onClick={showSidebar} />
-                </Link>
-            </div>
+        <header>
+            <Link to="#" className='menu-bars'>
+                <FaIcons.FaBars onClick={showSidebar} />
+            </Link>
+
+            <nav>
+                <ul className="time">
+                <li><h2 className="clock"><Clock /></h2></li>
+                </ul>
+            </nav>
+
+            <a className="brand-name" href="/#">Luie Jeans</a>
+        </header>
+
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className='nav-menu-items' onClick={showSidebar}>
                     <li className="navbar-toggle">
