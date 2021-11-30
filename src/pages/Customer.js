d9b0d58b1e604c90280f3604eea32fe5eaefed8d
TheLadsters <jeraldmags@gmail.com>
2021-12-01 03:33:26 +0800

    added customer landing page

---
+++ src/pages/Customer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+
+const Customer = () => {
+    return (
+        <>
+            <Navbar />  
+            <h1>HELLO! THIS IS THE CUSTOMER PAGE</h1>
+        </>
+    )
+}
+
+export default Customer
