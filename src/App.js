7b2f8d7b4a9ede2da2ae4f0fad86c09a72335d9d
TheLadsters <jeraldmags@gmail.com>
2021-12-01 03:36:03 +0800

    imported customer and staff pages, added routes for both also

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Login from "./components/Login";
 import LandingPage from "./pages/LandingPage";
 import OrderHistory from "./pages/OrderHistory";
 import Account from "./pages/Account";
+import Customer from "./pages/Customer";
+import Staff from "./pages/Staff";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router-dom";
@@ -15,6 +17,8 @@ const App = () => {
       <Route path="/landing-page" element={<LandingPage />} />
       <Route path="/order-history" element={<OrderHistory />} />
       <Route path="/account" element={<Account />} />
+      <Route path="/customer" element={<Customer />} />
+      <Route path="/staff" element={<Staff />} />
     </Routes>
     </>
   )
