9e618267822f32961994af79f45e12e145843131
TheLadsters <jeraldmags@gmail.com>
2021-12-01 22:57:50 +0800

    added new route for order status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Login from "./components/Login";
 import LandingPage from "./pages/LandingPage";
 import OrderHistory from "./pages/OrderHistory";
+import OrderStatus from "./pages/OrderStatus";
 import Account from "./pages/Account";
 import Customer from "./pages/Customer";
 import Staff from "./pages/Staff";
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/" element={<Login />} />
       <Route path="/landing-page" element={<LandingPage />} />
       <Route path="/order-history" element={<OrderHistory />} />
+      <Route path="/order-status" element={<OrderStatus />} />
       <Route path="/account" element={<Account />} />
       <Route path="/customer" element={<Customer />} />
       <Route path="/staff" element={<Staff />} />
