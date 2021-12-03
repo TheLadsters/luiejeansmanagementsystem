e19003fa45a517c103017a0942dc2eac702ecb2c
janmelmel <70429539+janmelmel@users.noreply.github.com>
2021-12-03 23:27:08 +0800

    .

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,7 +11,7 @@ const Mods = () => {
 
     return(
         <Container>
-            <Row className="justify-content-start" >
+            <Row className="justify-content-start m-0" >
                 <Container className="w-50 flex-row">
                     <FormControl    
                         className="w-50"
@@ -21,21 +21,22 @@ const Mods = () => {
                         size = "sm"
                     />
                 </Container>
-                <ButtonGroup as={Row} className="w-50 flex-row-reverse" size="sm">
-                    <ButtonGroup as={Row} className="w-50" size="sm">
+                <ButtonGroup as={Row} className="w-50 flex-row-reverse m-0" size="sm">
+                    <ButtonGroup className="w-50" as={Row} size="sm">
                         <Button className="w-50" variant = "primary">P</Button>
                         <Button className="w-50" variant = "secondary">D</Button>
                     </ButtonGroup>
+                    
                 </ButtonGroup>
             </Row>
-            <Container className="d-flex justify-content-center">
-                <Card style={{ width: '50%' }}>
+            <Container className="d-flex justify-content-center mt-10">
+                <Card style={{ width: '50%'},{margin:'5% 20% 5% 20%'}}>
                     <Nav>
                         <Nav.Link className="bg-image hover-overlay ripple shadow-1-strong" href="/"><Card.Img className="" variant="top" src={Upload} /></Nav.Link>
                     </Nav>
                     <ListGroup className="list-group-flush">
                         <FormControl
-                            placeholder="Order-Code"
+                            placeholder="Product Name"
                             aria-label="Username"
                             aria-describedby="basic-addon1"
                             size = "sm"
@@ -44,8 +45,8 @@ const Mods = () => {
                 </Card>
                     {/* <div className="upload-photo"src="holder.js/171x180" thumbnailxs={6} md={4}><h3>Upload Photo</h3></div>
                     <input className="form-control" type="text" placeholder="product_name" /> */}
-            </Container>
-            <Container className="d-flex justify-content-between">
+            </Container >
+            <Container className="d-flex justify-content-between" style={{width:'35%'},{margin:'0% 0% 5%'}}>
                 <Container className="d-inline-flex"    >
                     <label>Price</label>
                     <FormControl
@@ -73,14 +74,14 @@ const Mods = () => {
                 <InputGroup.Text> A </InputGroup.Text>
                 <FormControl id="inlineFormInputGroup" placeholder="Username" />
             </InputGroup> */}
-            <Container className="flex-row-start ">   
+            <Container className="flex-row-start" style={{margin:'10% 0% 10% 20%'},{padding:'0% 75% 0% 0%'}} >   
                 <InputGroup className="" size="sm">
                     <DropdownButton className="" variant="primary" size="sm"  id="dropdown-basic-button" title="Add Size">
                         <Row className="justify-content-between">
                             <Dropdown.Header>Kids</Dropdown.Header>
                             <Dropdown.Item as={Col} href="#/action-1" id="14">14</Dropdown.Item>
                             <Dropdown.Item as={Col} href="#/action-2" id="14">16</Dropdown.Item>
-                            <Dropdown.Item as={Col} href="#/action-3" id="14">18</Dropdown.Item>
+                            <Dropdown.Item as={Col} href="#/ac  tion-3" id="14">18</Dropdown.Item>
                             <Dropdown.Item as={Col} href="#/action-1" id="14">20</Dropdown.Item>
                             <Dropdown.Item as={Col} href="#/action-2" id="14">22</Dropdown.Item>
                             <Dropdown.Item as={Col} href="#/action-3" id="14">24</Dropdown.Item>
@@ -111,7 +112,7 @@ const Mods = () => {
                 </InputGroup>
             </Container>
 
-            <Container className="mt-0">
+            <Container className="mt-0" style={{padding:'10 % 0% 5% 0%'}}>
                 <Row>
                     <Col>Quantity:  10</Col>
                     <Col>Total: 1000</Col>
