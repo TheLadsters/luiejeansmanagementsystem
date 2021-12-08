da5ff5b92c3430feb44505f77501b1ccc6f4e9ed
yevgeny182 <94223536+yevgeny182@users.noreply.github.com>
2021-12-09 07:56:28 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import "react-phone-number-input/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
