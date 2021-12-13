35936dbbd7374c790168b35437a6e2e090238f1e
TheLadsters <jeraldmags@gmail.com>
2021-12-13 13:13:36 +0800

    commented out some unnecessary routes, added new const registerUser, authUser

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,24 +1,28 @@
 const router = require('express').Router();
+const { registerUser, authUser } = require('../controllers/userControllers');
 let User = require('../models/user.model');
 
-router.route('/').get((req, res) => {
-    User.find()
-    .then(users=>res.json(users))
-    .catch(err=>res.status(400).json('Error: ' + err));
-});
+// router.route('/').get((req, res) => {
+//     User.find()
+//     .then(users=>res.json(users))
+//     .catch(err=>res.status(400).json('Error: ' + err));
+// });
 
-router.route('/add').post((req, res) => {
-    const username = req.body.username;
-    const fullName = req.body.fullName;
+router.route("/").post(registerUser);
+router.route("/login").post(authUser);   
 
-    const newUser = new User({username,fullName});
+// router.route('/add').post((req, res) => {
+//     const email = req.body.email;
+//     const password = req.body.password;
+
+//     const newUser = new User({email,password});
     
 
     
 
-    newUser.save()
-    .then(() => res.json('User added!'))
-    .catch(err => res.status(400).json('Error' + err));
-});
+//     newUser.save()
+//     .then(() => res.json('User added!'))
+//     .catch(err => res.status(400).json('Error' + err));
+// });
 
 module.exports = router;
