badb420ced04a0b1f127992bdb225995c2e3af2e
TheLadsters <jeraldmags@gmail.com>
2021-12-15 17:26:12 +0800

    adjusted the routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,28 +1,17 @@
 const router = require('express').Router();
-const { registerUser, authUser } = require('../controllers/userControllers');
+const { registerUser, authUser, addTaskUser } = require('../controllers/userControllers');
 let User = require('../models/user.model');
 
-// router.route('/').get((req, res) => {
-//     User.find()
-//     .then(users=>res.json(users))
-//     .catch(err=>res.status(400).json('Error: ' + err));
-// });
+router.route('/').get((req, res) => {
+    User.find()
+    .then(users=>res.json(users))
+    .catch(err=>res.status(400).json('Error: ' + err));
+});
 
 router.route("/").post(registerUser);
-router.route("/login").post(authUser);   
+router.route("/login").post(authUser);
+router.route("/addTask").post(addTaskUser);    
 
-// router.route('/add').post((req, res) => {
-//     const email = req.body.email;
-//     const password = req.body.password;
-
-//     const newUser = new User({email,password});
-    
-
-    
-
-//     newUser.save()
-//     .then(() => res.json('User added!'))
-//     .catch(err => res.status(400).json('Error' + err));
-// });
+router.route('/add').post(registerUser);
 
 module.exports = router;
