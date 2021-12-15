9fadf924473615bdaac40c2a3116fdef65474ffb
TheLadsters <jeraldmags@gmail.com>
2021-12-15 17:26:40 +0800

    added the addTaskUser controller(unfinished)

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,7 +3,7 @@ const User = require('../models/user.model.js')
 const generateToken = require('../utils/generateToken');
 
 const registerUser = asyncHandler(async(req, res) => {
-    const { name, email, password, role, pic} = req.body;
+    const { name, email, password, role, pic, tasks} = req.body;
 
     const userExists = await User.findOne({email});
 
@@ -13,7 +13,7 @@ const registerUser = asyncHandler(async(req, res) => {
     }
 
     const user = await User.create({
-        name, email, password, role, pic
+        name, email, password, role, pic, tasks
     });
 
         if(user){
@@ -24,6 +24,7 @@ const registerUser = asyncHandler(async(req, res) => {
                 isAdmin: user.isAdmin,
                 role: user.role,
                 pic:user.pic,
+                tasks:user.tasks,
                 token: generateToken(user._id),
             });
         } else {
@@ -54,4 +55,24 @@ const authUser = asyncHandler(async (req, res) => {
     }
 });
 
-module.exports = { registerUser, authUser };
+const addTaskUser = asyncHandler(async (req, res) => {
+
+    const user = await User.findbyId( req.user._id );
+
+    if(user){
+        user.tasks = req.body.tasks ||user.tasks;
+
+        const updateUser = await user.save();
+
+        res.json({
+            _id: updateUser._id,
+            tasks: updateUser.tasks
+        })
+
+    } else{
+        res.status(404);
+        throw new Error("User not found!");
+    }
+});
+
+module.exports = { registerUser, authUser, addTaskUser };
