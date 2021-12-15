24f17bb6a7fe9daac756bbaa5bbf9817ea860a52
TheLadsters <jeraldmags@gmail.com>
2021-12-15 17:15:44 +0800

    added logic for getting all staff or user members

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,29 +1,60 @@
 import React, { useState } from 'react'
 import './Cards.css';
+import axios from 'axios';
 import { Card, Container, Col, Row, Modal, Button } from "react-bootstrap";
-const Cards = () => {
+const Cards = ( {staff} ) => {
 
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+    const handleShow = () => {
+        setShow(true);
+        console.log(staff._id);
+    } 
+    const [tasks, setTasks] = useState([]);
+
+
+    const addTask = async (e) => {
+        e.preventDefault();
+        
+            try{
+                const config = {
+                    headers: {
+                    "Content-type": "application/json",
+                    },
+                };
+
+
+            const { data } = await axios.post(
+                "http://localhost:5000/users/",
+                {
+                    tasks
+                },
+                config
+            );
+
+                localStorage.setItem("userInfo", JSON.stringify(data));
+ 
+
+            } catch (error){
+                console.log(error);
+            }
+    }
 
     return (
         <>
-            <Card style={{ width: '25rem', height:'14rem'}} className="real-card" onClick={handleShow}>
+            <Card style={{ width: '25rem', height:'10rem'}} className="real-card" onClick={handleShow}>
                         <Container>
                             <Card.Body>
                                 <Row>
-                                    <Col>
-                                        <Card.Img variant="top" style={{width:"8rem"}} src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" />
-                                        <Card.Title style={{textAlign:"center"}}>Jerald Magsipoc</Card.Title>
-                                        <Card.Text style={{textAlign:"center"}}>Tailor</Card.Text>
+                                    <Col align="center">
+                                        <Card.Img variant="top" style={{width:"8rem", height:"8rem"}} src={staff.pic} />
                                     </Col>
 
-                                    <Col> 
-                                        <Card.Text>
-                                        Some quick example text to build on the card title and make up the bulk of
-                                        the card's content.
+                                    <Col className="card-detail"> 
+                                        <Card.Text className="card-text">
+                                        <Card.Title>{staff.name}</Card.Title>
+                                        <Card.Text><b>{staff.role}</b></Card.Text>
                                         </Card.Text>
                                     </Col>
                                 </Row>
@@ -33,23 +64,33 @@ const Cards = () => {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>List of Things to Do</Modal.Title>
+                    <Modal.Title>Tasks Assigned</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
-                    <div className="form-check">
-                        <input className="form-check-input" disabled type="checkbox" defaultValue id="flexCheckDefault" />
+                    <Row>
+                        <Col>
+                            <Button variant="dark">
+                                Add task
+                            </Button>
+                        </Col>
+                    </Row>
+                
+                <Row className="py-4">
+                   <Col>
+                        <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
                         <label className="form-check-label" htmlFor="flexCheckDefault">
                             Default checkbox
                         </label>
-                    </div>
+                    </Col>
+                </Row>
                 </Modal.Body>
 
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="success" onClick={handleClose}>
                         Save Changes
                     </Button>
                 </Modal.Footer>
