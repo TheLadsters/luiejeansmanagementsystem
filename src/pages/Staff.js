3792f90988ee2351f0cf2f70faf64239aa4edabe
TheLadsters <jeraldmags@gmail.com>
2021-12-15 17:18:30 +0800

    added changes

--- src/pages/Staff.js
+++ src/pages/Staff.js
@@ -1,18 +1,124 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../components/Navbar';
-import { Button, Container, Col, Row, Modal } from "react-bootstrap";
+import { Button, Container, Col, Row, Modal, Form } from "react-bootstrap";
 import Cards from '../components/Cards';
 import * as IoIcons from 'react-icons/io';
+import Select from 'react-select';
+import Loading from "../components/Loading"
+import ErrorMessage from "../components/ErrorMessage"
+import axios from 'axios';
+import './Staff.css';
+
 
 
 const Staff = () => {
 
     // constants for add new staff modal
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [pic, setPic] = useState(
+    "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"
+    );
+    const [password, setPassword] = useState("");
+    const [confirmpassword, setConfirmPassword] = useState("");
+    const [message, setMessage] = useState(null);
+    const [picMessage, setPicMessage] = useState(null);
+    const [registerSuccess, setRegisterSuccess] = useState(null);
+    const [role, setRole] = useState("");
     // end of constants
+
+    function onChangeRole(role){
+        setRole(role.value);
+    }
+
+
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const Roles = [
+        { label: "Designer", value: "Designer" },
+        { label: "Sewer", value: "Sewer" },
+        { label: "Tailor", value: "Tailor" },
+        { label: "Packager", value: "Packager" },
+        { label: "Printer", value: "Printer" },
+    ]
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        
+        if (password !== confirmpassword){
+            setMessage("Passwords Do not Match");
+        } else{
+            setMessage(null)
+            try{
+                const config = {
+                    headers: {
+                    "Content-type": "application/json",
+                    },
+                };
+
+            setLoading(true)
+
+            const { data } = await axios.post(
+                "http://localhost:5000/users/",
+                {
+                    name,email,password, role, pic
+                },
+                config
+            );
+
+                setLoading(false);
+                localStorage.setItem("userInfo", JSON.stringify(data));
+                setRegisterSuccess("You have Registered Successfully!");
+ 
+
+            } catch (error){
+                setError(error.response.data.message);
+                setLoading(false)
+            }
+        }
+
+    }
+
+    const postDetails = (pics) => {
+        if(!pics) {
+            return setPicMessage("Please Select an Image");
+        }
+
+        setPicMessage(null);
+
+        if(pics.type === 'image/jpeg' || pics.type === 'image/png'){
+            const data = new FormData();
+            data.append('file', pics)
+            data.append('upload_preset', 'luiejeans')
+            data.append('cloud_name', 'dhvbaxacn')
+            fetch("https://api.cloudinary.com/v1_1/dhvbaxacn/image/upload", {
+                method:"post",
+                body:data,
+            })
+            .then((res) => res.json())
+            .then((data) => {
+                setPic(data.url.toString());
+                console.log("data succesfully uploaded");
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+        } else{
+            return setPicMessage("Please Select an Image");
+        }
+    };
+    const [staff, setStaff] = useState([]);
+    useEffect(() => {
+        axios
+        .get("http://localhost:5000/users")
+        .then(res => setStaff(res.data))
+        .catch(error => console.log(error));
+    },[setStaff]);
 
     return (
         <>
@@ -40,7 +146,7 @@ const Staff = () => {
             <Container className="my-4">
                 <Row>
                     <Col>
-                        <Button onClick={handleShow}>Add New Staff</Button>
+                        <Button variant ="success" onClick={handleShow}>Add New Staff</Button>
                     </Col>
                 </Row>
             </Container>
@@ -52,33 +158,64 @@ const Staff = () => {
                 </Modal.Header>
 
                 <Modal.Body>
-                    <div className="form-check">
-                        <input className="form-check-input" disabled type="checkbox" defaultValue id="flexCheckDefault" />
-                        <label className="form-check-label" htmlFor="flexCheckDefault">
-                            Default checkbox
-                        </label>
-                    </div>
-                </Modal.Body>
+                {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+                {message && <ErrorMessage variant="danger">{message}</ErrorMessage>}
+                {registerSuccess && <ErrorMessage variant="success">{registerSuccess}</ErrorMessage>}
+                {loading && <Loading />}  
+                <Form onSubmit={submitHandler}>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Enter Name" />
+                    </Form.Group>
 
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="success" onClick={handleClose}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control type="password" value={confirmpassword} onChange={(e) => setConfirmPassword(e.target.value)} placeholder="Confirm Password" />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicRole">
+                        <Form.Label>Role</Form.Label>
+                        <Select options={Roles} onChange={onChangeRole}/>
+                    </Form.Group>
+
+                    {picMessage && (<ErrorMessage variant="danger">{picMessage}</ErrorMessage>)}
+                    <Form.Group className="mb-3" controlId="formBasicProfilePicture">
+                        <Form.Label>Profile Picture</Form.Label>
+                        <Form.Control type="file" onChange={(e) => postDetails(e.target.files[0])} label="Upload Profile Picture" custom/>
+                    </Form.Group>
+
+
+                    <Button variant="success" type="submit">
                         Save Changes
                     </Button>
-                </Modal.Footer>
+                    
+                    <Button variant="secondary" className="mx-2" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Form>
+
+                </Modal.Body>
             </Modal>
             {/* End of modal */}
 
             {/* All cards for staff(should be .map)*/}
-            <Container>
-                <Row>
-                    <Col>
-                        <Cards />
-                    </Col>
-                </Row>
-            </Container>
+            <div className="staff-container container">
+                <div className="row">
+                    {staff.map((staff, key) => {
+                        return <div className="my-4"><Cards staff={staff}/></div>
+                    })}
+                </div>
+            </div>
             {/* end of cards */}
         </>
     )
