63f1712de576b2add1fee4d42af11a2e06de56d7
TheLadsters <jeraldmags@gmail.com>
2022-01-03 10:16:26 +0800

    added new route for landing page

---
+++ backend/routes/landingPage.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+let Customer = require('../models/customers.model');
+
+router.route('/').get((req, res) => {
+    Customer.find()
+    .then(customers=>res.json(customers))
+    .catch(err=>res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const customerFirstName = req.body.customerFirstName;
+    const customerLastName = req.body.customerLastName;
+    const numberOfOrders = req.body.numberOfOrders;
+    const contactNumber = req.body.contactNumber;
+    const customerAddress = req.body.customerAddress;
+    const productName = req.body.productName;
+    const orderDate = req.body.orderDate;
+    const price = req.body.price;
+    const orderCode = req.body.orderCode;
+    const productSize = req.body.productSize;
+    const deliveryType = req.body.deliveryType;
+    const productPic = req.body.productPic;
+    const downPayment = req.body.downPayment; 
+
+    const newCustomers = new Customer({
+        customerFirstName,
+        customerLastName,
+        numberOfOrders,
+        contactNumber,
+        customerAddress,
+        productName,
+        orderDate,
+        price,
+        orderCode,
+        productSize,
+        deliveryType,
+        productPic,
+        downPayment
+    });
+
+    newCustomers.save()
+    .then(() => res.json('Customer added!'))
+    .catch(err => res.status(400).json('Error' + err));
+});
+
+router.route('/:id').get((req, res) => {
+    Customer.findById(req.params.id)
+    .then(customers => res.json(customers))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').delete((req, res) => {
+    Customer.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Customer Deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Customer.findById(req.params.id)
+    .then(customer => {
+        customer.customerFirstName = req.body.customerFirstName;
+        customer.customerLastName = req.body.customerLastName;
+        customer.numberOfOrders = Number(req.body.numberOfOrders);
+        customer.contactNumber = req.body.contactNumber;
+
+        Customer.save()
+        .then( () => res.json('Product Updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+module.exports = router;
