60ae669ea90502157d8a8f01ae3d176bec7d1f90
TheLadsters <jeraldmags@gmail.com>
2022-01-03 10:18:02 +0800

    added new key and values in customers model schema

--- backend/models/customers.model.js
+++ backend/models/customers.model.js
@@ -5,8 +5,17 @@ const Schema = mongoose.Schema;
 const customersSchema = new Schema({
     customerFirstName:{ type: String, required: true},
     customerLastName:{ type: String, required: true},
-    numberOfOrders:{ type: Number, required: true},
-    contactNumber: {type: String, required: true}
+    numberOfOrders:{ type: Number},
+    contactNumber: {type: String, required: true},
+    customerAddress:{type:String},
+    productName: {type: String},
+    orderDate: {type: Date},
+    price: {type: Number},
+    orderCode: {type: String},
+    productSize: {type:String},
+    deliveryType: {type: String},
+    productPic: {type: String},
+    downPayment: {type: Number}
 
 }, {
     timestamps: true,
